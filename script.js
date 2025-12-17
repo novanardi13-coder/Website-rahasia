e5f597b13cda436b7bae00520a5ef904a7af2813
VANZZ_STORE <novanardi13@gmail.com>
2025-12-17 12:10:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const secretText = document.getElementById("secretText");
 
 secretText.addEventListener("click", () => {
-  window.location.href = "https://share.google/6mLQAy6Ju9rgJGYrq"; 
+  window.location.href = "https://share.google/8uiD4aWq9yqE3Z4ff"; 
   // ganti dengan link rahasia kamu
 });
