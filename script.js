72aca0856fe820b31fa50778d167d6d9c050e539
VANZZ_STORE <novanardi13@gmail.com>
2025-12-17 09:37:22 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const secretText = document.getElementById("secretText");
+
+secretText.addEventListener("click", () => {
+  window.location.href = "https://share.google/6mLQAy6Ju9rgJGYrq"; 
+  // ganti dengan link rahasia kamu
+});
